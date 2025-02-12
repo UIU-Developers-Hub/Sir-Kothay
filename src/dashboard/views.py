793b47ceac6de8ffc31ff9caf052e2856ec3aa81
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2025-02-12 13:34:25 +0600

    message crud done

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from broadcast.models import BroadcastMessage
+from .models import UserDetails
+from qrcodeApp.models import QRCode
 
 # Create your views here.
 @login_required(login_url='login')
 def home_view(request):
-    return render(request, 'dashboard/home.html')
+    user = user=request.user
+    messages = BroadcastMessage.objects.all().filter(user=user)
+    userd = UserDetails.objects.get(user=user)
+    
+    try:
+        qrcode = QRCode.objects.get(user=user)
+    except QRCode.DoesNotExist:
+        qrcode = None
+    
+    return render(request, 'dashboard/home.html', {
+        'messages': messages,
+        'userd': userd,
+        'qrcode': qrcode
+    })
 
 @login_required(login_url='login')
 def user_detail_view(request):
